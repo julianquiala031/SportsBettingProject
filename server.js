9824538ae354bf9ba5f12c46f82abb9fc278db88
UhIbraheem <aby2004.im@gmail.com>
2025-03-06 03:01:52 -0500

    Populating the ejs files with some bootstrap templates. Then making the register function functional and encrypted. Note- i used an arr to store user info we need to connect that to the database

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
-// Initiate the server/ grabbing imported modules
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt'); // importing bcrypt for password hashing
+const path = require('path');
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// TEMPORARILY STORING INFO IN ARR, MUST LINK TO DATABASE
+const users = [];
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch (e) {
+        res.redirect('/register');
+    }
+    // displaying newly registered users in the console
+    console.log(users);
+});
 
 // creating routes
 app.get('/', (req, res) => {
-    res.require('index.ejs');
+    res.render('index', { name: 'User' }); // Pass the name variable here
 });
 
 app.get('/login', (req, res) => {
-    res.require('login.ejs');
+    res.render('login', { messages: {} }); // Pass the messages variable here
 });
 
 app.get('/register', (req, res) => {
-    res.require('register.ejs');
+    res.render('register', { messages: {} }); // Pass the messages variable here
 });
-// End of routes
 
+
+// Server listening on port 3000
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
