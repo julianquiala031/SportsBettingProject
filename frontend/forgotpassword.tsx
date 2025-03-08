8b6348861168a8a30ef4ae78ad45f67a54ec3b6f
julianquiala031 <julianquiala031@gmail.com>
2025-03-07 20:10:18 -0500

    Basic logic for login and forgotpassword frontend

---
+++ frontend/forgotpassword.tsx
@@ -0,0 +1,31 @@
+import React, { use, useState } from "react";
+
+function forgotPassword(){
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+
+    const submit = (e) => {
+        e.preventDefault();
+
+        fetch('http://localhost:3000/auth/forgot-password',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(email),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Sending reset link to email: ', data);
+             //resets states to blank after submission
+            setEmail(''); 
+        })
+        .catch((error)=> {
+            console.error('There was an error in resetting your password: ', error);
+            setError('Failed to reset password.');
+        });
+
+    }
+
+}
+
