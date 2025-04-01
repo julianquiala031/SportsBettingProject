f25b9c142a80ea1019cc63344f51aa800ddb5865
julianquiala031 <julianquiala031@gmail.com>
2025-04-01 12:31:58 -0400

    Added socket to listen to constant game updates in frontend.

--- frontend/games.tsx
+++ frontend/games.tsx
@@ -1,7 +1,23 @@
 import React, {useState, useEffect} from 'react';
+import {io} from "socket.io-client"; 
+
+const socket = io("http://localhost:3000");
 
 function viewGames(){
-    
+    const [games, setGames] = useState([]);
+
+    useEffect(()=>{
+        socket.on("update", (updates) => {
+            setGames(updates);
+        });
+
+        return() => {
+            socket.off("update");
+        };
+    }, []);
 
 
 }
+
+
+export default viewGames;
